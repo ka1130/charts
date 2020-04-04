516b82a5f0e2368cfd97e1dc12151a9271a11dd9
Kamila Matla-Tomczyk <ka1130@o2.pl>
2020-04-04 17:08:37 +0200

    Add animation

--- src/BarCharts.jsx
+++ src/BarCharts.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import data from "./data";
-import { VictoryBar, VictoryChart, VictoryAxis } from "victory";
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryLabel } from "victory";
 
 const tickValues = Array.from(Array(data.length).keys());
 
 const BarCharts = () => (
   <div className="barCharts">
-    <h2>BarCharts</h2>
-    <VictoryChart>
+    <VictoryChart animate={{ duration: 500 }}>
       <VictoryAxis dependentAxis />
-      <VictoryAxis tickValues={tickValues} tickFormat={x => `Q-${x}`} />
+      <VictoryAxis
+        invertAxis
+        tickValues={tickValues}
+        tickFormat={x => `Q${x + 1}`}
+        style={{ tickLabels: { fontSize: 3 } }}
+      />
       <VictoryBar
         className="victoryBarWrapper"
+        horizontal
+        // padding={{ left: 20, right: 60 }}
+        barWidth={2}
         data={data}
         x="quarter"
         y="earnings"
